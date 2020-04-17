a5b2e9e3b657a991ebada7135df1e0234e9cbcfd
Robert Kuhlig <16413104+Kuhlig@users.noreply.github.com>
2020-04-17 18:00:11 +0200

    SOE

--- SOE/scripts/config_xml.js
+++ SOE/scripts/config_xml.js
@@ -2,8 +2,8 @@ var TSC = TSC || {};
 
 TSC.embedded_config_xml = '<x:xmpmeta tsc:version="2.0.1" xmlns:x="adobe:ns:meta/" xmlns:tsc="http://www.techsmith.com/xmp/tsc/">\
    <rdf:RDF xmlns:rdf="http://www.w3.org/1999/02/22-rdf-syntax-ns#" xmlns:xmp="http://ns.adobe.com/xap/1.0/" xmlns:xmpDM="http://ns.adobe.com/xmp/1.0/DynamicMedia/" xmlns:xmpG="http://ns.adobe.com/xap/1.0/g/" xmlns:xmpMM="http://ns.adobe.com/xap/1.0/mm/" xmlns:tscDM="http://www.techsmith.com/xmp/tscDM/" xmlns:tscIQ="http://www.techsmith.com/xmp/tscIQ/" xmlns:tscHS="http://www.techsmith.com/xmp/tscHS/" xmlns:stDim="http://ns.adobe.com/xap/1.0/sType/Dimensions#" xmlns:stFnt="http://ns.adobe.com/xap/1.0/sType/Font#" xmlns:exif="http://ns.adobe.com/exif/1.0" xmlns:dc="http://purl.org/dc/elements/1.1/">\
-      <rdf:Description dc:date="2020-04-14 09:06:36 " dc:source="Camtasia,19.0.10,deu" dc:title="SOE" tscDM:firstFrame="SOE_First_Frame.png" tscDM:originId="71C94241-333A-476D-80D0-18FC1D1CDDC0" tscDM:project="SOE">\
-         <xmpDM:duration xmpDM:scale="1/1000" xmpDM:value="140833"/>\
+      <rdf:Description dc:date="2020-04-17 11:35:18 " dc:source="Camtasia,19.0.10,deu" dc:title="SOE" tscDM:firstFrame="SOE_First_Frame.png" tscDM:originId="71C94241-333A-476D-80D0-18FC1D1CDDC0" tscDM:project="SOE">\
+         <xmpDM:duration xmpDM:scale="1/1000" xmpDM:value="290500"/>\
          <xmpDM:videoFrameSize stDim:unit="pixel" stDim:h="956" stDim:w="1700"/>\
          <tsc:langName>\
             <rdf:Bag>\
@@ -11,14 +11,6 @@ TSC.embedded_config_xml = '<x:xmpmeta tsc:version="2.0.1" xmlns:x="adobe:ns:meta
          </tsc:langName>\
          <xmpDM:Tracks>\
             <rdf:Bag>\
-               <rdf:li>\
-                  <rdf:Description xmpDM:trackType="Hotspot" xmpDM:frameRate="f1000" xmpDM:trackName="Hotspots">\
-                     <xmpDM:markers>\
-                        <rdf:Seq>\
-                           <rdf:li><rdf:Description xmpDM:label="1" xmpDM:startTime="1000" xmpDM:duration="133970" tscDM:boundingPoly="1637,887;1688,887;1688,937;1637,937;" tscDM:rotate="0.000000" tscHS:pause="1" xmpDM:location="https://www.bpmspace.com/https://www.techsmith.com" tscHS:newWindow="1"/></rdf:li><rdf:li><rdf:Description xmpDM:label="2" xmpDM:startTime="570" xmpDM:duration="27160" tscDM:boundingPoly="67,24;313,24;313,100;67,100;" tscDM:rotate="0.000000" tscHS:pause="1" xmpDM:location="https://prime73.exam.online/" tscHS:newWindow="1"/></rdf:li><rdf:li><rdf:Description xmpDM:label="3" xmpDM:startTime="29470" xmpDM:duration="61830" tscDM:boundingPoly="61,21;309,21;309,97;61,97;" tscDM:rotate="0.000000" tscHS:pause="1" xmpDM:location="https://prime73.exam.online/" tscHS:newWindow="1"/></rdf:li><rdf:li><rdf:Description xmpDM:label="4" xmpDM:startTime="91300" xmpDM:duration="48530" tscDM:boundingPoly="1409,884;1633,884;1633,952;1409,952;" tscDM:rotate="0.000000" tscHS:pause="1" xmpDM:location="https://prime73.exam.online/" tscHS:newWindow="1"/></rdf:li></rdf:Seq>\
-                     </xmpDM:markers>\
-                  </rdf:Description>\
-               </rdf:li>\
             </rdf:Bag>\
          </xmpDM:Tracks>\
          <tscDM:controller>\
